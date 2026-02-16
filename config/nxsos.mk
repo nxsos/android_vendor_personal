8b2add5673a3109e6900344aa1728c96dc419920
nyxiereal <98595166+nyxiereal@users.noreply.github.com>
2026-02-16 21:04:00 +0100

    add proper permissions and exempt some apps from battery optimizations
    Change-Id: Id8bb241535e44080b6d785d9aea9231e6d0924a2

--- config/nxsos.mk
+++ config/nxsos.mk
@@ -15,7 +15,11 @@ PRODUCT_PACKAGES += \
     KernelSUNext \
     Vending
 
-# Copy permission XMLs
+# Copy permission XMLs to both system and product partitions
 PRODUCT_COPY_FILES += \
-    vendor/nxsos/apps/permissions/permissions_com.aurora.services.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.aurora.services.xml \
-    vendor/nxsos/apps/permissions/privapp-permissions-com.chiller3.bcr.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-com.chiller3.bcr.xml
+    vendor/nxsos/apps/permissions/permissions_com.aurora.services.xml:system/etc/permissions/permissions_com.aurora.services.xml \
+    vendor/nxsos/apps/permissions/privapp-permissions-com.chiller3.bcr.xml:system/etc/permissions/privapp-permissions-com.chiller3.bcr.xml \
+    vendor/nxsos/apps/permissions/privapp-permissions-com.rosan.installer.x.revived.xml:system/etc/permissions/privapp-permissions-com.rosan.installer.x.revived.xml \
+    vendor/nxsos/apps/permissions/privapp-permissions-com.android.vending.xml:system/etc/permissions/privapp-permissions-com.android.vending.xml \
+    vendor/nxsos/apps/permissions/privapp-permissions-com.google.android.gms.xml:system/etc/permissions/privapp-permissions-com.google.android.gms.xml \
+    vendor/nxsos/apps/permissions/nxsos_whitelist.xml:system/etc/sysconfig/nxsos_whitelist.xml
