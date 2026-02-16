1e045764ae8b9345f53e66c84bd107ac57d66fc3
nyxiereal <98595166+nyxiereal@users.noreply.github.com>
2026-02-16 12:07:33 +0100

    add proper build files
    Change-Id: Ie543c400b7b46ed513c66ac601471e25c9f0fa1f

---
+++ config/nxsos.mk
@@ -0,0 +1,12 @@
+# NXSOS prebuilt apps and permissions
+PRODUCT_PACKAGES += \
+    AuroraServices \
+    BCR \
+    Droidify \
+    GmsCore \
+    GsfProxy \
+    InstallerXRevived \
+    KernelSUNext \
+    Vending \
+    permissions_com_aurora_services \
+    privapp_permissions_com_chiller3_bcr
